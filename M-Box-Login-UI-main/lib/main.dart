5747bb58224821cce1f311fff9b09a788c211fb0
zuhaibalikorejo <2k10cs89@gmail.com>
2023-07-13 19:05:24 +0400

    aldshflaksdf

--- M-Box-Login-UI-main/lib/main.dart
+++ M-Box-Login-UI-main/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 //import 'package:lgoin_ui/pages/task2.dart';
 import 'package:lgoin_ui/pages/task3.dart';
 
-//import 'pages/task1.dart';
+
 
 void main() {
   runApp(const MyApp());
